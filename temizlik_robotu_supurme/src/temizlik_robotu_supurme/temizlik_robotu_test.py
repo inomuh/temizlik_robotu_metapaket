4f4906632427eeacdbddf3f2ad8ad08492fe5116
HakanGENCTURK <hkn.gencturk@gmail.com>
2020-02-13 15:07:08 +0300

    Deleted

--- temizlik_robotu_supurme/src/temizlik_robotu_supurme/temizlik_robotu_test.py
+++
@@ -1,9 +0,0 @@
-import rospy
-from temizlik_robotu_supurme_dugumu import TemizlikRobotuSupurme
-
-
-robotum = TemizlikRobotuSupurme()
-
-gorev_listesi = list([[2, 0], [5, 90], [1, 180], [5,90], [1, 0], [5,90], [1, 180], [5,90], [1, 0], [5,90], [1, 180], [5,0]])
-robotum.gorev_atama_func(gorev_listesi)
-robotum.main_func()
